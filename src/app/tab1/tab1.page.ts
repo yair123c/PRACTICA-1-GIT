f770fd6de3bedb92b656660bda0293d5ece57a18
yair123c <christianj2328@gmail.com>
2023-01-25 09:03:35 -0600

    practica

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  constructor() {}
+ngOnInit(): void {
+  console.log("hola mundo en IONIC");
+
+  let numero= 20;
+  debugger;
+  numero = 10;
+  numero = 15 ;
+  numero = 100;
+
+  console.log("la variable esta tomando este valor: "+numero);
+}
+}
